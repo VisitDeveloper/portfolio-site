3d8bff553ecd41168609c48668f57a746b00792a
fire_full <amiralihatami21@gmail.com>
2024-03-02 13:13:02 +0200

    add data to peronal page

--- src/pages/ProjectsPageById.tsx
+++ src/pages/ProjectsPageById.tsx
@@ -1,12 +1,16 @@
 // import { RootState } from "@reduxjs/toolkit/query";
+import { Div, MainCard } from "components/_index";
 import { useAppSelector } from "store/hooks";
 
 const ProjectsPageById = () => {
-  const selector = useAppSelector((state : any) => state);
-  console.log("selector", selector);
-  return (<div>
-    
-  </div>);
+  const selector = useAppSelector((state: any) => state);
+  console.log("selector", selector.project.object);
+  return <Div>
+      <MainCard >
+      sjf
+      </MainCard>
+
+  </Div>;
 };
 
 export default ProjectsPageById;
